639817d507a5623043c54fb269a246e9dfe8be88
shtyh <92566646+shtyh@users.noreply.github.com>
2026-05-21 14:14:29 +0800

    delete files

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://dguohpdqwyfxlpurnwjw.supabase.co'
-const supabaseKey = 'sb_publishable_0ckNTrDF7EY0aDKjiLuhGw_XAANNNNt'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
